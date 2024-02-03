3447030e7bd5fc8cb095d42df3284c2b3bbabc84
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-02-03 13:19:14 -0300

    addiciona ErrorHandler para lidar com exceções da aplicação e altera server com o novo middleware de ErrorHandler

--- api/public/server.php
+++ api/public/server.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\handlers\ErrorHandler;
 use App\http\Router;
 use App\http\RouterCollector;
 use React\Http\HttpServer;
@@ -12,12 +13,15 @@ require_once __DIR__ . '/../src/routes/web.php';
 require_once __DIR__ . '/../services/services.php';
 
 $server = new HttpServer(
-    new Router(
-        RouterCollector::getInstance()
-    )
+    new ErrorHandler(),
+    new Router(RouterCollector::getInstance())
 );
 
 $socket = new SocketServer('0.0.0.0:7000');
 $server->listen($socket);
+
+$server->on('error', function (Throwable $throwable) {
+    echo 'Error: ' . $throwable->getMessage() . PHP_EOL;
+});
 
 echo 'Listening on ' . str_replace('tcp', 'http', $socket->getAddress()) . PHP_EOL . PHP_EOL;
---
+++ api/src/handlers/ErrorHandler.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\handlers;
+
+use App\http\Response;
+use Psr\Http\Message\ServerRequestInterface;
+
+class ErrorHandler
+{
+    public function __invoke(ServerRequestInterface $serverRequest, callable $next)
+    {
+        try {
+            return $next($serverRequest);
+        } catch (\Throwable $throwable) {
+            return Response::internalServerError($throwable->getMessage());
+        }
+    }
+}
