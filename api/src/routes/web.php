7b00309f43e028cef0d817c54311f53306a25d17
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-02-03 13:16:50 -0300

    addiciona novas rotas de products e orders

--- api/src/routes/web.php
+++ api/src/routes/web.php
@@ -1,11 +1,21 @@
 <?php
 
+use App\controllers\OrderController;
 use App\controllers\ProductController;
 use App\http\RouterCollector;
 
 $routeCollector = RouterCollector::getInstance();
 $routeCollector->addRoute('GET', '/products', [ProductController::class, 'index']);
 $routeCollector->addRoute('POST', '/products', [ProductController::class, 'store']);
+$routeCollector->addRoute('GET', '/products/{id:\d+}', [ProductController::class, 'show']);
+$routeCollector->addRoute('PUT', '/products/{id:\d+}', [ProductController::class, 'update']);
+$routeCollector->addRoute('DELETE', '/products/{id:\d+}', [ProductController::class, 'delete']);
+
+$routeCollector->addRoute('GET', '/orders', [OrderController::class, 'index']);
+$routeCollector->addRoute('POST', '/orders', [OrderController::class, 'store']);
+$routeCollector->addRoute('GET', '/orders/{id:\d+}', [OrderController::class, 'show']);
+$routeCollector->addRoute('PUT', '/orders/{id:\d+}', [OrderController::class, 'update']);
+$routeCollector->addRoute('DELETE', '/orders/{id:\d+}', [OrderController::class, 'delete']);
 
 /*$routeCollector->get('/products', function (ServerRequestInterface $request) {
     return new Response(
