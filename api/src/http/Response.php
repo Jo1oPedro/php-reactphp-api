fb83db9ee335670dbf815111e1ef87d5bc7b7253
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-02-03 13:18:09 -0300

    addiciona classe de response

---
+++ api/src/http/Response.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\http;
+use React\Http\Message\Response as ReactPhpResponse;
+
+class Response
+{
+    public function __invoke(int $statusCode, mixed $data = null): ReactPhpResponse
+    {
+        $data = $data ? json_encode($data) : null;
+
+        return new ReactPhpResponse(
+            $statusCode,
+            ['Content-type', 'application/json'],
+            $data
+        );
+    }
+
+    public static function ok($data): ReactPhpResponse
+    {
+        return new ReactPhpResponse(
+          200,
+          ['Content-type', 'application/json'],
+          $data
+        );
+    }
+
+    public static function error(int $code, string $reason): ReactPhpResponse
+    {
+        return new ReactPhpResponse(
+            $code,
+            ['Content-type' => 'application/json'],
+            json_encode(['message' => $reason])
+        );
+    }
+
+    public static function internalServerError(string $reason): ReactPhpResponse
+    {
+        return new ReactPhpResponse(
+            500,
+            ['Content-type' => 'application/json'],
+            json_encode(['message' => $reason])
+        );
+    }
+}
