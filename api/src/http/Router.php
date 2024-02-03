4d75226096bbdb2b5df6b521aa09aeb71c30bbe5
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-02-03 13:21:52 -0300

    refatora classe de router e adiciona método para lidar com injeção de dependencia dos métodos

--- api/src/http/Router.php
+++ api/src/http/Router.php
@@ -33,24 +33,58 @@ final class Router
         file_put_contents(__DIR__ . '/x.txt', 'valido: ' . $memcached->get('x') . ' x2:' . $this->x);
         $this->x++;
 
+        $routeInfo = $this->extractRouteInfo($serverRequest);
+
+        if(!is_array($routeInfo)) {
+            return $routeInfo;
+        }
+
+        [$handler, $vars] = $routeInfo;
+
+        if(is_array($handler)) {
+            [$controllerId, $method] = $handler;
+
+            $controller = Container::getInstance()->get($controllerId);
+            $handler = [$controller, $method];
+            $vars = $this->autoWireMethod($handler, $vars, $serverRequest);
+            return call_user_func_array($handler, $vars);
+        }
+    }
+
+    private function extractRouteInfo(ServerRequestInterface $serverRequest): Response|array
+    {
         $routeInfo = $this->dispatcher->dispatch(
             $serverRequest->getMethod(), $serverRequest->getUri()->getPath()
         );
 
-        try {
-            switch ($routeInfo[0]) {
-                case Dispatcher::NOT_FOUND:
-                    return new Response(404, ['Content-type' => 'text/plain'], json_encode('Not found'));
-                case Dispatcher::METHOD_NOT_ALLOWED:
-                    return new Response(405, ['Content-type' => 'text/plain'], json_encode('Method not allowed'));
-                case Dispatcher::FOUND:
-                    $controller = Container::getInstance()->get($routeInfo[1][0]);
-                    return $controller->{$routeInfo[1][1]}($routeInfo[2]);
-                default:
-                    throw new LogicException('Something went wrong with routing');
-            }
-        } catch (Exception $exception) {
-            dd($exception);
+        switch ($routeInfo[0]) {
+            case Dispatcher::NOT_FOUND:
+                return new Response(404, ['Content-type' => 'text/plain'], json_encode('Not found'));
+            case Dispatcher::METHOD_NOT_ALLOWED:
+                return new Response(405, ['Content-type' => 'text/plain'], json_encode('Method not allowed'));
+            case Dispatcher::FOUND:
+                return [$routeInfo[1], $routeInfo[2]];
+            default:
+                throw new LogicException('Something went wrong with routing');
         }
+    }
+
+    private function autoWireMethod($handler, $vars, ServerRequestInterface $serverRequest)
+    {
+        $reflectionController = new ReflectionClass($handler[0]);
+        $reflectionMethod = $reflectionController->getMethod($handler[1]);
+        foreach($reflectionMethod->getParameters() as $parameter) {
+            if(array_key_exists($parameter->name, $vars)) {
+                continue;
+            }
+
+            if($parameter->getType()->getName() === 'Psr\Http\Message\ServerRequestInterface') {
+                $vars[$parameter->name] = $serverRequest;
+                continue;
+            }
+
+            $vars[$parameter->name] = Container::getInstance()->get($parameter->getType()->getName());
+        }
+        return $vars;
     }
 }
