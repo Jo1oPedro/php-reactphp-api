28a6248cf6f99760cfbb5f88775b0d73a8a50e35
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-02-03 13:22:19 -0300

    refatora as responses do productController

--- api/src/controllers/ProductController.php
+++ api/src/controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\controllers;
 
 use App\rabbitmq\RabbitmqManager;
+use Psr\Http\Message\ServerRequestInterface;
 use React\Http\Message\Response;
 
 class ProductController extends Controller
@@ -12,7 +13,7 @@ class ProductController extends Controller
         //dd($x);
     }
 
-    public function index()
+    public function index(x $x, ServerRequestInterface $serverRequest)
     {
         RabbitmqManager::publishMessage(
             'produto_registrado',
@@ -32,6 +33,33 @@ class ProductController extends Controller
             200,
             ['Content-type' => 'application/json'],
             json_encode(['message' => 'POST request to /products'])
+        );
+    }
+
+    public function show(int $id)
+    {
+        return new Response(
+            200,
+            ['Content-type' => 'application/json'],
+            json_encode(['message' => "GET request to /products/{$id}"])
+        );
+    }
+
+    public function update(int $id)
+    {
+        return new Response(
+            200,
+            ['Content-type' => 'application/json'],
+            json_encode(['message' => "PUT request to /products/{$id}"])
+        );
+    }
+
+    public function delete(int $id)
+    {
+        return new Response(
+            200,
+            ['Content-type' => 'application/json'],
+            json_encode(['message' => "DELETE request to /products/{$id}"])
         );
     }
 }
