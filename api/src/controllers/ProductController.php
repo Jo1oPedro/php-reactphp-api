6c8d800068dd0bea27cc2d8111e7f249f3b2eafc
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-02-05 15:48:39 -0300

    adiciona métodos de store e show ao productController

--- api/src/controllers/ProductController.php
+++ api/src/controllers/ProductController.php
@@ -2,18 +2,22 @@
 
 namespace App\controllers;
 
+use App\entitys\Product as ProductEntity;
+use App\exceptions\ProductNotFound;
+use App\mappers\Product as ProductMapper;
+use App\repositories\Product as ProductRepository;
 use App\rabbitmq\RabbitmqManager;
 use Psr\Http\Message\ServerRequestInterface;
 //use React\Http\Message\Response;
 use App\http\Response;
 class ProductController extends Controller
 {
-    public function __construct(x $x)
-    {
-        //dd($x);
-    }
+    public function __construct(
+        private ProductMapper $productMapper,
+        private ProductRepository $productRepository
+    ) {}
 
-    public function index(x $x, ServerRequestInterface $serverRequest)
+    public function index(ServerRequestInterface $serverRequest)
     {
         RabbitmqManager::publishMessage(
             'produto_registrado',
@@ -28,26 +32,40 @@ class ProductController extends Controller
 
     public function store(ServerRequestInterface $request)
     {
-        $product = [
-            'name' => $request->getParsedBody()['name'],
-            'price' => $request->getParsedBody()['price']
-        ];
+        $name = $request->getParsedBody()['name'];
+        $price =$request->getParsedBody()['price'];
 
-        return Response::ok(
+        return $this->productMapper
+            ->save(new ProductEntity(name: $name, price: $price))
+            ->then(
+                function (ProductEntity $product) {
+                    return Response::ok(json_encode($product));
+                },
+                function (\Exception $exception) {
+                    return Response::internalServerError($exception->getMessage());
+                }
+            );
+
+        /*return Response::ok(
             json_encode([
                 'message' => 'POST request to /products',
                 'product' => $product
             ]),
-        );
+        );*/
     }
 
     public function show(int $id)
     {
-        return new Response(
-            200,
-            ['Content-type' => 'application/json'],
-            json_encode(['message' => "GET request to /products/{$id}"])
-        );
+        return $this->productRepository->getById($id)
+            ->then(
+                function (ProductEntity $product) {
+                    return Response::ok(json_encode($product));
+                },
+            )->catch(function (ProductNotFound $productNotFound) {
+                return Response::notFound();
+            })->catch(function (\Exception $exception) {
+                return Response::internalServerError($exception->getMessage());
+            });
     }
 
     public function update(int $id)
