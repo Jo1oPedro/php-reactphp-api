3de26ab47552dcfe1c240a328a8d8bcba57e5c93
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-02-05 15:46:56 -0300

    altera diretorio das configurações do container

--- api/public/server.php
+++ api/public/server.php
@@ -14,36 +14,13 @@ define('BASE_PATH', dirname(__DIR__));
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../src/routes/web.php';
-require_once __DIR__ . '/../services/services.php';
+require_once __DIR__ . '/../config/services.php';
 
 $middlewares = [
     new ErrorHandler(),
     new JsonRequestDecoder(),
     new Router(RouterCollector::getInstance())
 ];
-
-$container = Container::getInstance();
-
-$serviceIp = gethostbyname('banco_de_dados_relacional');
-
-// SÓ É POSSÍVEL CONECTAR AO MYSQL LATEST OU 8+ UTILIZANDO ESSE CONNECTOR DEVIDO A ALGUNS PROBLEMAS DA BIBLIOTECA
-$connector = new Connector([
-    'dns' => $serviceIp,
-    'tcp' => [
-        // We have to set this correct, otherwise you get that error:
-        // https://github.com/friends-of-reactphp/mysql/issues/112
-        'bindto' => "{$serviceIp}:3306",
-    ],
-    'tls' => [
-        'verify_peer' => false,
-        'verify_peer_name' => false,
-    ]
-]);
-
-$mysql = new Factory(connector: $connector);
-$connection = $mysql->createLazyConnection(
-"{$container->get('MYSQL_USER')}:{$container->get('MYSQL_PASSWORD')}@{$serviceIp}:3306/{$container->get('MYSQL_DATABASE')}"
-);
 
 $server = new HttpServer(...$middlewares);
 
